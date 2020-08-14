cbb71c489f4cae29247192359de88e3005b4c0ef
MarcoYcaza <ycaza.marco@gmail.com>
2020-08-14 13:16:57 -0500

    test

--- Retrofit/src/main/java/TheAPI.java
+++ Retrofit/src/main/java/TheAPI.java
@@ -5,10 +5,6 @@ import java.util.List;
 
 public interface TheAPI {
 
-    //String key = "84afbdd4bbmsh8357420c470620fp11eabdjsnf5d7dd810be4";
-    //'X-RapidAPI-Key=84afbdd4bbmsh8357420c470620fp11eabdjsnf5d7dd810be4'
-   // @Header("X-RapidAPI-Key");
-    //@HEAD("X-RapidAPI-Key=84afbdd4bbmsh8357420c470620fp11eabdjsnf5d7dd810be4")
     @GET("weather")
     Call<elementito> loadelementitos(@Query("q") String city);
 
